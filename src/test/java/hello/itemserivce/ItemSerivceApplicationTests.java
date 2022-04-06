9695d9bfb9972e0d10ee150cc14b123dd0739948
syc <syc@partdb.com>
2022-04-06 21:43:37 +0900

    Add: 프로젝트 생성 @tabutabu98

---
+++ src/test/java/hello/itemserivce/ItemSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
